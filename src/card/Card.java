54cf8042ce33da82ace4d3b6798445a5a0a77f2f
윤하겸 <younha3710@naver.com>
2025-02-25 16:48:28 +0900

    🔥Ex.java 삭제

--- src/card/Card.java
+++ src/card/Card.java
@@ -3,8 +3,7 @@ package src.card;
 import java.util.ArrayList;
 import java.util.List;
 
-// Card 클래스.
-// 2025-02-24 작성
+// Card 클래스
 public class Card {
     private CardShape shape; // 카드 문양
     private int number; // 카드 숫자 (1~13, 조커는 예외)
