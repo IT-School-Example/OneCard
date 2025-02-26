135966d8d2cbeb1eb52204fb6e70413853451d36
jee13 <jee1358@naver.com>
2025-02-26 16:31:18 +0900

    Computer.java - 수정

--- src/player/Computer.java
+++ src/player/Computer.java
@@ -1,7 +1,16 @@
 package src.player;
 
+import src.card.Card;
+import src.card.CardDeck;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Computer extends Player {
-    // private int computer; // 컴퓨터 플레이어 수
+    private int computer; // 컴퓨터 플레이어 수
+
+    public List<Card> handCard = new ArrayList<>();
 
     public Computer(String name) {
         super(name);
@@ -11,5 +20,23 @@ public class Computer extends Player {
         return this;
     }
 
+    public List<Card> getHandCards() { return handCard; }
 
+    // 카드 가져오기 (핸드 카드 플러스), 카드 공격당하거나, 낼게 없을때.
+    public void draw(CardDeck cardDeck) {
+        handCard.add(cardDeck.draw());
+    }
+
+    // 카드 내기 (핸드 카드 마이너스)
+    public Card drawOut(int index) {
+        return handCard.remove(((int) Math.random() * handCard.size())+1); // 보류
+    }
+
+    // 컴퓨터 N 플레이어
+    public int[] ComputerPlayers() {
+        Scanner sc = new Scanner(System.in);
+        this.computer = sc.nextInt();
+        int data [] = new int [computer];
+        return data;
+    }
 }
